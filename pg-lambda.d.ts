60c85d757ae9a78fd3ad0fb7f000db37bd818ae2
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2020-10-09 19:20:43 +0530

    Fixed some typings

--- pg-lambda.d.ts
+++ pg-lambda.d.ts
@@ -1,6 +1,6 @@
 // Hand written type definitions for pg-que 0.0.4
 import * as pg from 'pg-promise';
-import * as pgque from 'pg-que';
+import { PgQueue } from 'pg-que';
 
 interface StateStore {
     readerPG: pg.IDatabase<any>,
@@ -8,16 +8,16 @@ interface StateStore {
 }
 
 interface StartOptions {
-    maxsteps: integer,
-    readFrequency: integer,
-    messageAcquiredTimeout: integer,
-    retry: integer
+    maxsteps: number,
+    readFrequency: number,
+    messageAcquiredTimeout: number,
+    retry: number
 }
 
 export default class PgLambda {
-    constructor(name: string, inputQ: pgque, outputQ: pgque, expression: string, stateStore: StateStore, workers?: integer);
-    async startProcessing(options?: StartOptions);
-    async stopProcessing();
-    async dispose();
-    async state():Promise<Object>;
+    constructor(name: string, inputQ: PgQueue, outputQ: PgQueue, expression: string, stateStore: StateStore, workers?: number);
+    startProcessing(options?: StartOptions): Promise<void>;
+    stopProcessing(): Promise<void>;
+    dispose(): Promise<void>;
+    state(): Promise<Object>;
 }
